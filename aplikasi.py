9d38cb1d38ac3440dd3561e9214076502ab5feaf
AlifnurFathurrahmanPrasodjo <111824164+AlifnurFathurrahmanPrasodjo@users.noreply.github.com>
2023-06-15 16:54:34 +0700

    fix

--- aplikasi.py
+++ aplikasi.py
@@ -1,14 +1,8 @@
 # import libary 
 import streamlit as st
-from pathlib import Path
 import pandas as pd
 import numpy as np
-import joblib
 import matplotlib.pyplot as plt
-import streamlit.components.v1 as components
-from datetime import datetime
-import matplotlib.dates as mdates
-import calendar
 
 # pige title
 st.set_page_config(
@@ -236,13 +230,15 @@ with tab3:
         st.pyplot(plt)
 
         st.write("MAPE = ",error)
+    indexModel = np.argmin(errors)
+    svr = models[indexModel]
     
 
 with tab4:
     # Mengimpor model dan text dari file yang telah diekspor sebelumnya
-    svr = joblib.load('modelSVM.pkl')
+    # svr = joblib.load('modelSVM.pkl')
     df = pd.read_csv('DatasetSaham.csv')
-    scaler = joblib.load('scalerUAS.pkl')
+    # scaler = joblib.load('scalerUAS.pkl')
     dataset_test = pd.read_csv('data-test-UAS.csv')
 
 
@@ -255,41 +251,23 @@ with tab4:
         
         col1, col2 = st.columns(2)
 
-        tanggal = df['Date'].tail(1).values
-        tanggal = tanggal[0].split('-')
-        tahun = tanggal[0]
-        bulan = tanggal[1]
-        hari = tanggal[2]
-        tahun = int(tahun)
-        bulan = int(bulan)
-        hari = int(hari)
-        jumlah_hari = calendar.monthrange(tahun, bulan)[1]
+       
         n_pred = 1
         last = dataset_test.tail(1)
         fitur = last.values
         n_fit = len(fitur[0])
         fitur = fitur[:, 1:n_fit]
-        # input1 = fitur[0]
-        # input2 = fitur[1]
-        # input3 = fitur [2]
         y_pred=svr.predict(fitur)
         tahuns = np.zeros(n_pred)
         preds = np.array(y_pred)
-        hari += 1
-        if hari > jumlah_hari:
-            bulan += 1
-            hari = 1
-        if bulan > 12:
-            tahun += 1
-            bulan = 1
+        
 
-        tanggal = str(tahun)+"-"+f"{bulan:02d}"+"-"+f"{hari:02d}"
 
         reshaped_data = preds.reshape(-1, 1)
         original_data = scaler.inverse_transform(reshaped_data)
         pred = original_data.flatten()
         # print(original_data)
-        df_pred = pd.DataFrame({'Date': tanggal, 'Open': pred})
+        # df_pred = pd.DataFrame({'Date': tanggal, 'Open': pred})
 
         with col1:
             last_three_rows = df['Open'].tail(3).values
