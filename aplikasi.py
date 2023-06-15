52b19b709a6c78f0f2311f0efa2da1e9a3b3ab9d
ThoriqFathu <thoriq771@gmail.com>
2023-06-15 13:49:18 +0700

    update streamlit

--- aplikasi.py
+++ aplikasi.py
@@ -3,95 +3,91 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 import joblib
-
-def ramal(n_pred, dataset_test, tahun):
-    tahun = tahun[0]
-    last = dataset_test.tail(1)
-    fitur = last.values
-    n_fit = len(fitur[0])
-    fiturs = np.zeros((n_pred, n_fit))
-    tahuns = np.zeros(n_pred)
-    preds = np.zeros(n_pred)
-    for i in range(n_pred):
-        if i == 0:
-            fitur = fitur[:, 1:n_fit]
-            y_pred=svr.predict(fitur)
-            new_fit = np.array(fitur[0])
-            new_fit = np.append(new_fit,y_pred)
-        else:
-            fitur = fiturs[i-1][1:n_fit]
-            y_pred=svr.predict([fitur])
-            new_fit = np.array(fitur)
-            new_fit = np.append(new_fit,y_pred)
-        preds[i] = y_pred
-        fiturs[i,:] = new_fit 
-        tahun += 1
-        tahuns[i] = tahun
-#     print(preds)
-#     print(fiturs)
-    return preds, tahuns.astype(int)
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+from datetime import datetime
+import calendar
 
 
 # Mengimpor model dan text dari file yang telah diekspor sebelumnya
 svr = joblib.load('modelSVM.pkl')
-df = pd.read_csv('tempe.txt', sep=',', names=['Year', 'Anomaly'])
+df = pd.read_csv('INDF.JK.csv')
 scaler = joblib.load('scaler.pkl')
-dataset_test = pd.read_csv('data-test.csv')
+dataset_test = pd.read_csv('data-test-UAS.csv')
 
 
 
 
 # Judul aplikasi
-st.markdown("<h1 style='text-align: center;'>Forecasting Temperature Anomalies Data Time Series</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center;'>Forecasting Harga Saham PT. INDOFOOD</h1>", unsafe_allow_html=True)
 # st.title('Forecasting Temperature Anomalies Data Time Series')
 
-# Define a range for the slider
-min_value = 0
-max_value = 60
-
-# Create a slider widget
-selected_value = st.slider('Select a value', min_value, max_value)
-
-# Display the selected value
-st.write('You selected:', selected_value)
 
 # Tombol untuk memprediksi
 if st.button('Run'):
-    if selected_value:
-        col1, col2 = st.columns(2)
-        tahun_terakhir = df['Year'].tail(1).values
-        pred, tahun = ramal(selected_value,dataset_test, tahun_terakhir)
-        # print(pred)
-        # print(tahun)
-        reshaped_data = pred.reshape(-1, 1)
-        original_data = scaler.inverse_transform(reshaped_data)
-        # print(original_data)
-        df_pred = pd.DataFrame({'YEAR': tahun, 'ANOMALY': pred})
-        # st.dataframe(df_pred)
-        # st.write('Hasil Prediksi:', prediction)
+    
+    col1, col2 = st.columns(2)
 
-        # Plot data df
-        fig, ax = plt.subplots()
-        ax.plot(df['Year'], df['Anomaly'], label='Data Awal')
+    tanggal = df['Date'].tail(1).values
+    tanggal = tanggal[0].split('-')
+    tahun = tanggal[0]
+    bulan = tanggal[1]
+    hari = tanggal[2]
+    tahun = int(tahun)
+    bulan = int(bulan)
+    hari = int(hari)
+    jumlah_hari = calendar.monthrange(tahun, bulan)[1]
+    n_pred = 1
+    last = dataset_test.tail(1)
+    fitur = last.values
+    n_fit = len(fitur[0])
+    fitur = fitur[:, 1:n_fit]
+    y_pred=svr.predict(fitur)
+    tahuns = np.zeros(n_pred)
+    preds = np.array(y_pred)
+    hari += 1
+    if hari > jumlah_hari:
+        bulan += 1
+        hari = 1
+    if bulan > 12:
+      tahun += 1
+      bulan = 1
 
-        # Plot data df_pred
-        ax.plot(df_pred['YEAR'], df_pred['ANOMALY'], label='Prediksi')
+    tanggal = str(tahun)+"-"+f"{bulan:02d}"+"-"+f"{hari:02d}"
 
-        # Menghubungkan plot terakhir data awal dengan plot awal data prediksi
-        last_year = df['Year'].iloc[-1]
-        ax.plot([last_year, df_pred['YEAR'].iloc[0]], [df['Anomaly'].iloc[-1], df_pred['ANOMALY'].iloc[0]], 'k--')
+    reshaped_data = preds.reshape(-1, 1)
+    original_data = scaler.inverse_transform(reshaped_data)
+    pred = original_data.flatten()
+    # print(original_data)
+    df_pred = pd.DataFrame({'Date': tanggal, 'Open': pred})
 
-        # Konfigurasi plot
-        ax.set_xlabel('Tahun')
-        ax.set_ylabel('Anomali')
-        ax.set_title('Perbandingan Data Awal dan Prediksi')
-        ax.legend()
+    # Plot data df
 
-        # Tampilkan plot
-        # st.pyplot(fig)
-        with col1:
-            st.dataframe(df_pred)
-        with col2:
-            st.pyplot(fig)
-    else:
-        st.write('Masukkan range tahun terlebih dahulu')
+
+
+
+    # Plot data df
+    plt.plot(df['Date'], df['Open'], label='Data Awal')
+
+    # Plot data df_pred
+    plt.plot(df_pred['Date'], df_pred['Open'], label='Prediksi')
+
+    # Menghubungkan plot terakhir data awal dengan plot awal data prediksi
+    last_year = df['Date'].iloc[-1]
+    plt.plot([last_year, df_pred['Date'].iloc[0]], [df['Open'].iloc[-1], df_pred['Open'].iloc[0]], 'k--')
+
+    # Konfigurasi plot
+    plt.xlabel('Date')
+    plt.ylabel('Open')
+    plt.title('Perbandingan Data Awal dan Prediksi')
+    plt.legend()
+
+    plt.xticks(rotation=45, ha='right')
+    plt.gca().xaxis.set_major_locator(mdates.MonthLocator())
+
+    # Tampilkan plot
+    # st.pyplot(fig)
+    with col1:
+        st.dataframe(df_pred)
+    with col2:
+        st.pyplot(fig)
